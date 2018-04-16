5f1a858f93e36a00f24aa1ad8781bdb6fe0ab399
evans <evansqqlove@outlook.com>
2018-04-16 14:47:50 +0800

    Notice表新增title字段，活动通知模块完成

--- QingXie/src/main/java/com/selfcreate/qingxie/controller/system/NoticeController.java
+++ QingXie/src/main/java/com/selfcreate/qingxie/controller/system/NoticeController.java
@@ -1,9 +1,16 @@
 package com.selfcreate.qingxie.controller.system;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.selfcreate.qingxie.bean.Msg;
 import com.selfcreate.qingxie.bean.system.Notice;
+import com.selfcreate.qingxie.exception.QingxieInnerException;
+import com.selfcreate.qingxie.service.system.NoticeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * @author evans 2018/4/11 9:30
@@ -12,6 +19,8 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/notice")
 @Controller
 public class NoticeController {
+    @Autowired
+    NoticeService noticeService;
     /**
      * 发布公告
      * @return
@@ -19,7 +28,12 @@ public class NoticeController {
     @ResponseBody
     @RequestMapping(value = "/add",method = RequestMethod.POST)
     public Msg releaseNotice(@RequestBody Notice record){
-        throw new RuntimeException();
+        try {
+            noticeService.pushNotice(record);
+            return Msg.success("发布成功");
+        } catch (QingxieInnerException e) {
+            return Msg.error(e.getMessage());
+        }
     }
 
     /**
@@ -28,18 +42,31 @@ public class NoticeController {
      */
     @ResponseBody
     @RequestMapping(value = "/list",method = RequestMethod.GET)
-    public Msg getNotices(){
-        throw new RuntimeException();
+    public Msg getNotices(@RequestParam(value = "page", defaultValue = "1") int page,
+                          @RequestParam(value = "size", defaultValue = "8") int size){
+        try{
+            PageHelper.startPage(page, size);
+            List<Notice> notices=noticeService.getNotices();
+            PageInfo pageInfo=new PageInfo(notices);
+            return Msg.success("获取成功").add("PageInfo", pageInfo);
+        }catch (QingxieInnerException e){
+            return Msg.error(e.getMessage());
+        }
     }
 
     /**
      * 获取公告详情
-     * @param userId
+     * @param noticeId
      * @return
      */
     @ResponseBody
-    @RequestMapping(value = "/{noticeId}/list",method = RequestMethod.GET)
-    public Msg getHoursDetailsOfActivity(@PathVariable("noticeId") int userId){
-        throw new RuntimeException();
+    @RequestMapping(value = "/{noticeId}/details",method = RequestMethod.GET)
+    public Msg getDetailsById(@PathVariable("noticeId") int noticeId){
+        try {
+            Notice result=noticeService.getDetails(noticeId);
+            return Msg.success("获取成功").add("Notice", result);
+        } catch (QingxieInnerException e) {
+            return Msg.error(e.getMessage());
+        }
     }
 }
